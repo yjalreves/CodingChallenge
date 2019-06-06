1a4e4b5315d6aae0705eb573c1c2914c7291ed34
yjalreves <51414191+yjalreves@users.noreply.github.com>
2019-06-05 23:53:54 -0600

    Added DB Scripts for commit

---
+++ CodingChallengeDB/0002_INSERTS_USERS.sql
@@ -0,0 +1,28 @@
+USE [CodingChallenge]
+GO
+
+INSERT INTO [dbo].[Users]
+           ([FirstName]
+           ,[LastName])
+     VALUES
+           ("Jose"
+           ,"Salas")
+GO
+
+INSERT INTO [dbo].[Users]
+           ([FirstName]
+           ,[LastName])
+     VALUES
+           ("Alex"
+           ,"Ujueta")
+GO
+
+INSERT INTO [dbo].[Users]
+           ([FirstName]
+           ,[LastName])
+     VALUES
+           ("Johanna"
+           ,"Alfaro")
+GO
+
+
